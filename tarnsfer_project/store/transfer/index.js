fc8e4220710156ae6356c782ea59c6b01edd69b8
tawat <tawat.114.17@gmail.com>
2023-05-04 20:44:42 +0700

    advisor

--- tarnsfer_project/store/transfer/index.js
+++ tarnsfer_project/store/transfer/index.js
@@ -3,85 +3,78 @@ import axios from "axios";
 /* states */
 
 export const state = () => ({
-    equivalentCourse: [],
-    equivalentCourseByID: []
+  equivalentCourse: [],
+  equivalentCourseByID: [],
 });
 
 /* getters */
-export const getters = {
-
-
-};
-
+export const getters = {};
 
 /* mutations */
 export const mutations = {
-    getEquivalentCourse(state, data) {
-        state.equivalentCourse = {
-            ...state.equivalentCourse,
-            ...data,
-        };
-    },
-    getEquivalentCourseByID(state, data) {
-        state.equivalentCourseByID = {
-            ...state.equivalentCourseByID,
-            ...data,
-        };
-    },
+  getEquivalentCourse(state, data) {
+    state.equivalentCourse = {
+      ...state.equivalentCourse,
+      ...data,
+    };
+  },
+  getEquivalentCourseByID(state, data) {
+    state.equivalentCourseByID = {
+      ...state.equivalentCourseByID,
+      ...data,
+    };
+  },
 };
 
 /* actions */
 
 export const actions = {
-    async getEquivalentCourse({ commit, rootState }) {
-        var self = this;
-        let userID = rootState.users.userId
-        let role = rootState.users.userLogin.user.role.role
-        console.log("rolerolerolerolerole ", role);
+  async getEquivalentCourse({ commit, rootState }) {
+    var self = this;
+    let userID = rootState.users.userId;
+    let role = rootState.users.userLogin.user.role.role;
+    console.log("rolerolerolerolerole ", role);
 
-        if (role == "student") {
-            await this.$fixedKeyApi
-                .get(`/equivalent-course-all/?created_user=${userID}`)
-                .then(response => {
-                    self.search = response.data;
-                    console.log("getEquivalentCourse", response.data);
+    if (role == "student") {
+      await this.$fixedKeyApi
+        .get(`/equivalent-course-all/?created_user=${userID}`)
+        .then((response) => {
+          self.search = response.data;
+          console.log("getEquivalentCourse", response.data);
 
-                    commit("getEquivalentCourse", response.data);
-                });
-        } else {
-            await this.$fixedKeyApi
-                .get(`/equivalent-course-all/?search=${userID}`)
-                .then(response => {
-                    self.search = response.data;
-                    console.log("getEquivalentCourse", response.data);
+          commit("getEquivalentCourse", response.data);
+        });
+    } else {
+      await this.$fixedKeyApi
+        .get(`/equivalent-course-all/?advisor=${userID}`)
+        .then((response) => {
+          self.search = response.data;
+          console.log("getEquivalentCourse", response.data);
 
-                    commit("getEquivalentCourse", response.data);
-                });
-        }
+          commit("getEquivalentCourse", response.data);
+        });
+    }
+  },
 
+  async getEquivalentCourseByID({ commit }, { id }) {
+    var self = this;
+    // console.log("getProduct", productSearch);
 
-    },
+    await this.$fixedKeyApi
+      .get(`/equivalent-course/${id}/`)
+      .then((response) => {
+        self.search = response.data;
+        console.log("getEquivalentCourseByID", response.data);
 
-    async getEquivalentCourseByID({ commit }, { id }) {
-        var self = this;
-        // console.log("getProduct", productSearch);
-
-        await this.$fixedKeyApi
-            .get(`/equivalent-course/${id}/`)
-            .then(response => {
-                self.search = response.data;
-                console.log("getEquivalentCourseByID", response.data);
-
-                commit("getEquivalentCourseByID", response.data);
-            });
-    },
-
+        commit("getEquivalentCourseByID", response.data);
+      });
+  },
 };
 
 export default {
-    namespaced: true,
-    state,
-    getters,
-    mutations,
-    actions,
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
 };
